aaab6c7848e725985c1cc26835618a781320bad9
ADAM <ADAM@DESKTOP-4QLP97Q>
2025-04-25 03:20:26 +0300

    Add project files.

---
+++ First Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace First_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite number between 1 and 100:");
+            int age = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine($"No really!!, {age} is my favorite number too!");
+        }
+    }
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int SC = 25;
+            int LC = 35;
+            double Tax = 6.0/100.0;
+
+
+            Console.WriteLine("Number of small carpets: ");
+            double nomSc = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Number of large carpets: ");
+            double nomLc = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Price per small room: $25");
+
+            Console.WriteLine("Price per large room: $35");
+
+            double finalCostSc = nomSc * SC;
+            double finalCostLC = nomLc * LC;
+
+            double Cost = finalCostSc + finalCostLC;
+            Console.WriteLine($"Cost: {Cost} ");
+
+            double finalTax = Tax * Cost;
+            Console.WriteLine($"Tax: {Tax * Cost} ");
+
+            double TE = Cost + finalTax;
+            Console.WriteLine($"Total estimate: {TE} ");
+            Console.WriteLine("This estimate is valid for 30 days");
+
+
+
+
+
+
+
+        }
+    }
+}
